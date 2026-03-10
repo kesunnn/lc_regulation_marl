6abde48a47583aeaec00ef8d88044cbe53a082ab
kesun <kesun@your.hostname.com>
2026-03-10 11:32:10 -0500

    new env with buffer

--- pde_rl_control/environments/__init__.py
+++ pde_rl_control/environments/__init__.py
@@ -4,3 +4,4 @@ register(id='TrafficEnv_lane5_1', entry_point='pde_rl_control.environments.lane5
 register(id='TrafficEnv_lane5_2', entry_point='pde_rl_control.environments.lane5_2.traffic_env:Traffic_Env_Four_Action')
 register(id='TrafficEnv_lane5_3', entry_point='pde_rl_control.environments.lane5_3.traffic_env:Traffic_Env_Four_Action_With_Baseline')
 register(id='TrafficEnv_lane5_4', entry_point='pde_rl_control.environments.lane5_4.traffic_env:Traffic_Env_Four_Action_Aggressive_With_Baseline')
+register(id='TrafficEnv_lane5_5', entry_point='pde_rl_control.environments.lane5_5.traffic_env:Traffic_Env_Four_Action_Aggressive_With_Baseline_Buffered')
